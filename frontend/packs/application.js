db8f17defc80366f06042bdb7cbc988c8fa082f7
Anthony Lionnet <lionnet.ant@gmail.com>
2018-10-30 15:03:43 +0100

    Setting messages component up

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -2,6 +2,3 @@ import "init";
 import "components/page/page";
 import "components/auth-form/auth-form";
 import "components/chat/chat";
-import "components/message/message";
-import "components/messages/messages";
-import "components/message-form/message-form";
