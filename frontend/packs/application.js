c44fcf2f0a9891f1e97f2a8a56bddcc4f61214fb
Anthony Lionnet <lionnet.ant@gmail.com>
2018-10-30 14:19:33 +0100

    Adding message-form component

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -3,3 +3,5 @@ import "components/page/page";
 import "components/auth-form/auth-form";
 import "components/chat/chat";
 import "components/message/message";
+import "components/messages/messages";
+import "components/message-form/message-form";
